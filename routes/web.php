538696491b8eff9e65b92a43006d93e0b73dc2ba
daniloferreirasousa <daniloasf@outlook.com>
2024-01-13 15:54:04 -0300

    Adicionar Rotas de testes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+
+
+Route::get('/pagination', function(User $user) {
+    $filter = request('name');
+    $totalPage = request('paginate', 10);
+
+    $users = $user->where('name', 'LIKE', "%{$filter}%")->paginate($totalPage);
+
+    return $users;
+});
+
+Route::get('/where', function () {
+
+    $filterOne = 'be';
+    $filter = 'Da';
+
+    // $users = User::where('name', '<>', 'Danilo')->get();
+    // $users = User::findOrFail(request('id'));
+    $users = User::where('name', 'LIKE', "%{$filterOne}%")
+                    ->orWhere(function ($query) use($filter) {
+                        $query->where('name', 'LIKE', "%{$filter}%");
+                    })
+                    ->get();
+
+    dd($users);
+});
+
+Route::get('/select', function () {
+    $users = User::where('id', 1)->get()->first();
+
+    dd($users);
+});
 
 Route::get('/', function () {
     return view('welcome');
