1ade075d33b9b249eb9ee0f9dc5bdc6f716d0de9
daniloferreirasousa <daniloasf@outlook.com>
2024-01-13 12:48:17 -0300

    Adicionar Comentários à Model Posts

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -14,4 +14,19 @@ class Posts extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+
+    /*
+    *  Tipos de manipulações que podem ser implementadas
+    */
+    // protected $table = 'postagens'; # Serve p/ especificar o nome da tabela
+    // protected $primaryKey = 'id_postagem'; # Serve p/ especificar qual é a chave primária da tabela
+    // protected $keytype = 'string'; # serve p/ especificar o tipo de dado tem a coluna de chave primária
+    // protected $incrementing = false; # serve p/ definir que a chave primária não será auto-increment
+    // protected $timestamps = false; # serve p/ definir que não serão criadas as colunas de create e update
+    // const CREATED_AT = 'data_criacao'; # serve p/ renomear a coluna CREATED_AT
+    // const UPDATED_AT = 'data_atualizacao'; # serve p/ renomear a coluna UPDATED_AT
+    // protected $dateFormat = 'd/m/Y'; # serve p/ definir o formato que será salvo para as datas
+    // protected $connection = 'pgsql'; # serve p/ definir qual conexão com DB que a tabela irá utilizar
+    // protected $attributes = [ 'active' => true ]; # serve p/ definir valores padrão para colunas
 }
